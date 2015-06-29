258184f2f8977b5248eff27108708f843c3e2287
Brandon Myers <bdmyers@cs.washington.edu>
2015-06-29 10:59:59 -0700

    add parts and lineitem

---
+++ catalog.py
@@ -0,0 +1,31 @@
+# Schemas corresponding to Myrial examples
+
+{
+    'public:adhoc:part' : [
+      ('p_partkey', 'LONG_TYPE'), 
+      ('p_name', 'STRING_TYPE'),
+      ('p_mfgr', 'STRING_TYPE'),
+      ('p_brand', 'STRING_TYPE'),
+      ('p_size', 'LONG_TYPE'),
+      ('p_container', 'STRING_TYPE'),
+      ('p_retailprice', 'DOUBLE_TYPE'),
+      ('p_comment', 'STRING_TYPE')],
+
+    'public:adhoc:lineitem' : [
+      ('l_orderkey', 'LONG_TYPE'),
+      ('l_partkey', 'LONG_TYPE'),
+      ('l_suppkey', 'LONG_TYPE'),
+      ('l_linenumber', 'LONG_TYPE'),
+      ('l_quantity', 'LONG_TYPE'),
+      ('l_extendedprice', 'DOUBLE_TYPE'),
+      ('l_discount', 'DOUBLE_TYPE'),
+      ('l_tax', 'DOUBLE_TYPE'),     
+      ('l_returnflag', 'STRING_TYPE'), # only needs to be size 1
+      ('l_linestatus', 'STRING_TYPE'), # only needs to be size 1
+      ('l_shipdate', 'LONG_TYPE'), # DATE
+      ('l_commitdate', 'LONG_TYPE'), # DATE
+      ('l_receiptdate', 'LONG_TYPE'), # DATE
+      ('l_shipinstruct', 'STRING_TYPE'),
+      ('l_shipmode', 'STRING_TYPE'),
+      ('l_comment', 'STRING_TYPE')]
+}
