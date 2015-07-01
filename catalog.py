3fbc8bba179ad663e5f7e06750092c6e1b8134df
Brandon Myers <bdmyers@cs.washington.edu>
2015-07-01 12:57:50 -0700

    make dates strings instead of int

--- catalog.py
+++ catalog.py
@@ -1,16 +1,6 @@
 # Schemas corresponding to Myrial examples
 
 {
-    'public:adhoc:part' : [
-      ('p_partkey', 'LONG_TYPE'), 
-      ('p_name', 'STRING_TYPE'),
-      ('p_mfgr', 'STRING_TYPE'),
-      ('p_brand', 'STRING_TYPE'),
-      ('p_size', 'LONG_TYPE'),
-      ('p_container', 'STRING_TYPE'),
-      ('p_retailprice', 'DOUBLE_TYPE'),
-      ('p_comment', 'STRING_TYPE')],
-
     'public:adhoc:lineitem' : [
       ('l_orderkey', 'LONG_TYPE'),
       ('l_partkey', 'LONG_TYPE'),
@@ -22,10 +12,21 @@
       ('l_tax', 'DOUBLE_TYPE'),     
       ('l_returnflag', 'STRING_TYPE'), # only needs to be size 1
       ('l_linestatus', 'STRING_TYPE'), # only needs to be size 1
-      ('l_shipdate', 'LONG_TYPE'), # DATE
-      ('l_commitdate', 'LONG_TYPE'), # DATE
-      ('l_receiptdate', 'LONG_TYPE'), # DATE
+      ('l_shipdate', 'STRING_TYPE'), # DATE
+      ('l_commitdate', 'STRING_TYPE'), # DATE
+      ('l_receiptdate', 'STRING_TYPE'), # DATE
       ('l_shipinstruct', 'STRING_TYPE'),
       ('l_shipmode', 'STRING_TYPE'),
-      ('l_comment', 'STRING_TYPE')]
+      ('l_comment', 'STRING_TYPE')],
+
+    'public:adhoc:part' : [
+      ('p_partkey', 'LONG_TYPE'), 
+      ('p_name', 'STRING_TYPE'),
+      ('p_mfgr', 'STRING_TYPE'),
+      ('p_brand', 'STRING_TYPE'),
+      ('p_size', 'LONG_TYPE'),
+      ('p_container', 'STRING_TYPE'),
+      ('p_retailprice', 'DOUBLE_TYPE'),
+      ('p_comment', 'STRING_TYPE')],
+
 }
