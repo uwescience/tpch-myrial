12e4979702d5e06624abd3a7c052cb2ce9b67105
Brandon Myers <bdmyers@cs.washington.edu>
2015-09-02 18:00:59 -0700

    changes to make reproducibility easier

--- do_query.py
+++ do_query.py
@@ -20,10 +20,11 @@ if __name__ == "__main__":
 
     p = argparse.ArgumentParser(prog=sys.argv[0])
     p.add_argument("querynum", help="tpch query number")
-    p.add_argument("--no-regenerate", action="store_true", dest="no_regenerate", help="Do not re-run Raco on the query")
+    p.add_argument("--no-regenerate", action="store_true", dest="no_regenerate", help="Do not re-run Raco on the query if the exe already exists")
     p.add_argument("--compiler", dest="compiler", default="push", help="Radish compilation strategy {push,iterator}")
     p.add_argument("--build-mode", dest="build_mode", default="Release", help="Grappa build mode {Release,Debug}")
     p.add_argument("--make-parallelism", dest="make_parallelism", default="8", help="Argument to make -j")
+    p.add_argument("--configure-command", dest="configure", default="grappa_configure", help="command to configure grappa on your system; defaults to 'grappa_configure'")
 
     args = p.parse_args(sys.argv[1:])
 
@@ -34,10 +35,12 @@ if __name__ == "__main__":
     else:
         source_name = 'grappa_tpc_{}'.format(query)
 
+    build_dir = "{GRAPPA_HOME}/build/Make+{mode}/applications/join".format(GRAPPA_HOME=GRAPPA_HOME, mode=args.build_mode)
+
     with pushd(RACO_HOME):
 
         # Radish
-        if not args.no_regenerate:
+        if (not args.no_regenerate) or (not os.path.isfile("{d}/{f}.exe".format(d=build_dir, f=source_name))):
             subprocess.check_call("""PYTHONPATH=. scripts/myrial -c \
             --catalog {TPCH_RADISH}/catalog.py \
             {TPCH_RADISH}/{query}.myl \
@@ -53,11 +56,11 @@ if __name__ == "__main__":
                        GRAPPA_HOME=GRAPPA_HOME,
                        source_name=source_name), shell=True)
 
-    with pushd("{GRAPPA_HOME}/build/Make+{mode}/applications/join".format(GRAPPA_HOME=GRAPPA_HOME, mode=args.build_mode)):
+    with pushd(build_dir):
 
         # compile cpp
         if not os.path.isfile("{}.exe".format(source_name)):
-            subprocess.check_call("grappa_configure", shell=True)
+            subprocess.check_call("{c} --mode={mode}".format(c=args.configure, mode=mode), shell=True)
 
         subprocess.check_call("make -j {make_parallelism} {source_name}.exe".format(make_parallelism=args.make_parallelism, source_name=source_name),
                               shell=True)
