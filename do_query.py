173b7373a99af1bdc575573e35139069fb61eacf
Brandon Myers <bdmyers@cs.washington.edu>
2016-06-07 13:35:41 -0700

    some things that are useful like timing, and error checking

--- do_query.py
+++ do_query.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 from contextlib import contextmanager
 import os
+from timeit import repeat
 
 @contextmanager
 def pushd(newDir):
@@ -24,9 +25,11 @@ if __name__ == "__main__":
     p.add_argument("--join-type", dest="join_type", default="GrappaHashJoin", help="Radish join type")
     p.add_argument("--build-mode", dest="build_mode", default="Release", help="Grappa build mode {Release,Debug}")
     p.add_argument("--groupby-semantics", dest="groupby_semantics", default="global", help="{global, partition}")
-    p.add_argument("--make-parallelism", dest="make_parallelism", default="8", help="Argument to make -j")
+    p.add_argument("--make-parallelism", dest="make_parallelism", default="", help="Argument to make -j")
     p.add_argument("--configure-command", dest="configure", default="grappa_configure", help="command to configure grappa on your system; defaults to 'grappa_configure'")
     p.add_argument("--plan-info", action="store_true", dest="plan_info", help="whether to store plan info to plan.db")
+    p.add_argument("--generate-only", action="store_true", dest="generate_only", help="only run Raco")
+    p.add_argument("--timing", action="store_true", dest="timing", help="keep timing")
 
     args = p.parse_args(sys.argv[1:])
 
@@ -34,8 +37,10 @@ if __name__ == "__main__":
 
     if args.compiler == 'iterator':
         source_name = 'grappa_tpc_iter_{}'.format(query)
-    else:
+    elif args.compiler == 'push':
         source_name = 'grappa_tpc_{}'.format(query)
+    else:
+        raise ValueError("unsupported value for compiler: {}".format(args.compiler))
 
     if args.join_type == 'GrappaSymmetricHashJoin':
         source_name = source_name + '_sym'
@@ -51,19 +56,25 @@ if __name__ == "__main__":
 
         # Radish
         if (not args.no_regenerate) or (not os.path.isfile("{d}/{f}.exe".format(d=build_dir, f=source_name))):
-            subprocess.check_calll("""PYTHONPATH=. scripts/myrial -c \
-            --catalog {TPCH_RADISH}/catalog.py {plan_info} \
-            {TPCH_RADISH}/{query}.myl \
-            --key=scan_array_repr --value=symmetric_array \
-            --key=compiler --value={compiler} \
-            --key=join_type --value={join_type} \
-            --key=groupby_semantics --value={groupby_semantics} 
-            """.format(TPCH_RADISH=TPCH_RADISH,
-                       query=query,
-                       plan_info="--plan-info=plan.db" if args.plan_info else "",
-                       compiler=args.compiler,
-                       join_type=args.join_type,
-                       groupby_semantics=args.groupby_semantics), shell=True)
+            def raco():
+                subprocess.check_call("""PYTHONPATH=. scripts/myrial -c \
+                --catalog {TPCH_RADISH}/catalog.py {plan_info} \
+                {TPCH_RADISH}/{query}.myl \
+                --key=scan_array_repr --value=symmetric_array \
+                --key=compiler --value={compiler} \
+                --key=join_type --value={join_type} \
+                --key=groupby_semantics --value={groupby_semantics} 
+                """.format(TPCH_RADISH=TPCH_RADISH,
+                           query=query,
+                           plan_info="--plan-info=plan.db" if args.plan_info else "",
+                           compiler=args.compiler,
+                           join_type=args.join_type,
+                           groupby_semantics=args.groupby_semantics), shell=True)
+
+            if args.timing:
+                raco_times = repeat(raco, repeat=3, number=1)
+            else:
+                raco()
 
             subprocess.check_call("""mv {query}.cpp \
             {GRAPPA_HOME}/applications/join/{source_name}.cpp
@@ -71,14 +82,49 @@ if __name__ == "__main__":
                        GRAPPA_HOME=GRAPPA_HOME,
                        source_name=source_name), shell=True)
 
-    with pushd(build_dir):
+    if not args.generate_only:
+        with pushd(build_dir):
 
-        # compile cpp
-        if not os.path.isfile("{}.exe".format(source_name)):
-            subprocess.check_call("{c} --mode={mode}".format(c=args.configure, mode=args.build_mode), shell=True)
+            # compile cpp
+            if not os.path.isfile("{}.exe".format(source_name)):
+                subprocess.check_call("{c} --mode={mode}".format(c=args.configure, mode=args.build_mode), shell=True)
 
-        subprocess.check_call("make -j {make_parallelism} {source_name}.exe".format(make_parallelism=args.make_parallelism, source_name=source_name),
-                              shell=True)
+            def grappa_compile():
+                subprocess.check_call("make -j {make_parallelism} {source_name}.exe".format(make_parallelism=args.make_parallelism, source_name=source_name),
+                        shell=True)
+
+            if args.timing:
+                # do it once to compile dependences
+                grappa_compile()
+
+                def touch_first():
+                    subprocess.check_call("touch {GRAPPA_HOME}/applications/join/{source_name}.cpp".format(query=query, GRAPPA_HOME=GRAPPA_HOME, source_name=source_name), shell=True)
+                    grappa_compile()
+
+                grappa_compile_times = repeat(touch_first, repeat=3, number=1)
+            else:
+                grappa_compile()
 
 
+
+    if args.timing:
+        import dataset
+        db = dataset.connect('sqlite:///compilation.db')
+        params = { 'query': query,
+                   'compiler': args.compiler,
+                   'join_type': args.join_type,
+                   'groupby_semantics': args.groupby_semantics,
+                   }
+
+        for t in raco_times:
+            d = params.copy()
+            d.update({'raco_time': t}) # timeit gets the min
+            db['raco'].insert(d)
+            
+        compiler_version = subprocess.check_output('gcc -v 2>&1 | grep version', shell=True)
+        for t in grappa_compile_times:
+            d = params.copy()
+            d.update({'grappa_compile_time': t, 
+                      'compiler_version': compiler_version})
+            db['compiler'].insert(d)
 
